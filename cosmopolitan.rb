125f1639533766994de83f61e91fd51b246275db
Alex Nisnevich <alex.nisnevich@gmail.com>
2015-09-02 11:21:14 -0700

    more cities

--- cosmopolitan.rb
+++ cosmopolitan.rb
@@ -9,7 +9,12 @@ system("padding.sh")
 search_for_skylines([
   "Bombay", "Jakarta", "Karachi", "Moscow",
   "Istanbul", "MexicoCity", "Shanghai", "Tokyo", 
-  "NewYork", "Bangkok", "Beijing", "Delhi"
+  "NewYork", "Bangkok", "Beijing", "Delhi",
+  "London", "HongKong", "Cairo", "Tehran",
+  "Bogota", "Bandung", "Tianjin", "Lima",
+  "RioDeJaneiro", "Lahore", "Bogor", "Santiago",
+  "StPetersburg", "Shenyang", "Calcutta", "Wuhan",
+  "Sydney", "Guangzhou"
 ])
 
 create_video([ 
@@ -83,5 +88,68 @@ create_video([
   ["black.png", 8],
 
   ["processed_images/Delhi.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/London.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/HongKong.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Cairo.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Tehran.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Bogota.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Bandung.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Tianjin.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Lima.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/RioDeJaneiro.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Lahore.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Bogor.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Santiago.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/StPetersburg.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Shenyang.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Calcutta.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Wuhan.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Sydney.png", 24], 
+  ["black.png", 8],
+
+  ["processed_images/Guangzhou.png", 24], 
+  ["black.png", 8], 
+
+  ["padded/cosmo_padded_560.png", 12],  
+  ["padded/cosmo_padded_640.png", 12], 
+  ["black.png", 8], 
+
+  ["padded/politan_padded_720.png", 8],
+  ["padded/politan_padded_800.png", 8], 
+  ["padded/politan_padded_840.png", 8], 
   ["black.png", 8]
 ], fps, audio_path)
